28af1ad2a41002daf71cf4c8ecb7f89d01e2a475
salma-issis <salmaelyajizi@gmail.com>
2026-05-16 03:09:54 +0100

    first commit

---
+++ src/main/java/com/example/model/Utilisateur.java
@@ -0,0 +1,37 @@
+package com.example.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "utilisateurs")
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Le nom est obligatoire")
+    @Column(nullable = false)
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    @Column(nullable = false)
+    private String prenom;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Format d'email invalide")
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "utilisateur", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public Utilisateur(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+}
